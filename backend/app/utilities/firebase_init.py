494a7b1b4161e5c689a75831934a0f91b720bbde
mmiqball <mmiqbal@uwaterloo.ca>
2024-11-07 21:19:20 -0500

    simplify firebase initialization

--- backend/app/utilities/firebase_init.py
+++ backend/app/utilities/firebase_init.py
@@ -5,27 +5,5 @@ from firebase_admin import credentials
 
 
 def initialize_firebase():
-    private_key = os.getenv("FIREBASE_SVC_ACCOUNT_PRIVATE_KEY")
-    if private_key:
-        private_key = private_key.replace("\\n", "\n")
-
-    cred = credentials.Certificate(
-        {
-            "type": "service_account",
-            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-            "private_key_id": os.getenv("FIREBASE_SVC_ACCOUNT_PRIVATE_KEY_ID"),
-            "private_key": private_key,
-            "client_email": os.getenv("FIREBASE_SVC_ACCOUNT_CLIENT_EMAIL"),
-            "client_id": os.getenv("FIREBASE_SVC_ACCOUNT_CLIENT_ID"),
-            "auth_uri": os.getenv("FIREBASE_SVC_ACCOUNT_AUTH_URI"),
-            "token_uri": os.getenv("FIREBASE_SVC_ACCOUNT_TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv(
-                "FIREBASE_SVC_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL"
-            ),
-            "client_x509_cert_url": os.getenv(
-                "FIREBASE_SVC_ACCOUNT_CLIENT_X509_CERT_URL"
-            ),
-        }
-    )
-
+    cred = credentials.Certificate("llsc/backend/app/utilities/serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
