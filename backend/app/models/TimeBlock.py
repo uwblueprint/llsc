5ad9a7855bcf65828a1560714e7679aebbb91e5e
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-11-11 20:20:55 -0500

    create TimeBlock model
    To Do:
    - add relationship for Schedule (time_blocks field)

---
+++ backend/app/models/TimeBlock.py
@@ -0,0 +1,17 @@
+import uuid
+
+from sqlalchemy import Column, ForeignKey, Integer, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from .Base import Base
+
+class TimeBlock(Base):
+    __tablename__ = "time_blocks"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    schedule_id = Column(Integer, ForeignKey("schedules.id"), nullable = False)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+
+    schedule = relationship("Schedule", back_populates="time_blocks")
+
