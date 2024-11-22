55d88482099613caa90789b97b677e30a6071e87
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-11-22 17:56:38 -0500

    update schedule model

--- backend/app/models/Schedule.py
+++ backend/app/models/Schedule.py
@@ -1,25 +1,19 @@
 import uuid
 
-from sqlalchemy import Column, DateTime, Enum, Interval
+from sqlalchemy import Column, DateTime, Enum, Interval, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
 from .Base import Base
 
-schedule_state_enum = Enum(
-    "PENDING_PARTICIPANT_RESPONSE",
-    "PENDING_VOLUNTEER_RESPONSE",
-    "SCHEDULED",
-    "COMPLETED",
-    name = "state"
-)
 
 class Schedule(Base):
     __tablename__ = "schedules"
 
-    scheduleId = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    scheduledTime = Column(DateTime, nullable = False)
-    duration = Column(Interval, nullable = False)
-    state = Column(schedule_state_enum, nullable = False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    scheduled_time = Column(DateTime, nullable = True)
+    duration = Column(Interval, nullable = True)
+    state_id = Column(Integer, ForeignKey("schedule_states.id"), nullable=False)
 
+    state = relationship("State")
     time_blocks = relationship("TimeBlock", back_populates="schedule")
--- backend/app/models/ScheduleState.py
+++ backend/app/models/ScheduleState.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 from .Base import Base
 
 
-class Role(Base):
+class ScheduleState(Base):
     __tablename__ = "schedule_states"
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
