bb2411439fa7c428255146105fcc09e109640668
Rohan Nankani <rohan.nankani2021@gmail.com>
2025-03-10 20:32:48 -0400

    fix linting errors

--- backend/app/models/Matches.py
+++ backend/app/models/Matches.py
@@ -1,27 +1,28 @@
+import enum
 import uuid
 
-from sqlalchemy import Column, ForeignKey, DateTime, Enum, Integer, String
+from sqlalchemy import Column, DateTime, Enum, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from sqlalchemy.sql import func
 
-import enum
-
 from .Base import Base
+
 
 class MatchStatus(enum.Enum):
     PENDING_ADMIN_APPROVAL = "pending_admin_approval"
     APPROVED = "approved"
     REJECTED = "rejected"
 
+
 class ScheduleStatus(enum.Enum):
     SCHEDULED = "scheduled"
     COMPLETED = "completed"
-    DECLINED = "declined" # This status is for when a user declines a scheduled match
+    DECLINED = "declined"  # This status is for when a user declines a scheduled match
     PENDING_PARTICIPANT_RESPONSE = "pending_participant_response"
     PENDING_VOLUNTEER_RESPONSE = "pending_volunteer_response"
-    CANCELLED = "cancelled" # This status is for when a match is cancelled by an admin
+    CANCELLED = "cancelled"  # This status is for when a match is cancelled by an admin
+
 
 class Matches(Base):
     __tablename__ = "matches"
@@ -31,11 +32,15 @@ class Matches(Base):
     participant_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     volunteer_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
 
-    match_status = Column(Enum(MatchStatus), nullable=True, default=MatchStatus.PENDING_ADMIN_APPROVAL)
+    match_status = Column(
+        Enum(MatchStatus), nullable=True, default=MatchStatus.PENDING_ADMIN_APPROVAL
+    )
     schedule_status = Column(Enum(ScheduleStatus), nullable=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
 
     participant = relationship("User", foreign_keys=[participant_id])
     volunteer = relationship("User", foreign_keys=[volunteer_id])
