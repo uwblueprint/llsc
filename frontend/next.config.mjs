59f4b64937d530d572b727b4bb2762e7c8937f4a
janealsh <janealshihabi@gmail.com>
2025-03-11 21:42:32 -0400

    linting

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
+    optimizePackageImports: ['@chakra-ui/react'],
   },
 };
 
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from "@/components/ui/provider";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Provider } from '@/components/ui/provider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
