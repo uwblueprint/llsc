088dd6c517ff61f7fd8eee9087d42530d96f0c0f
janealsh <89433386+janealsh@users.noreply.github.com>
2025-03-11 21:45:18 -0400

    Set up chakra ui for frontend (#28)
    ## Notion ticket link
    <!-- Please replace with your ticket's URL -->
    [Ticket
    Name](https://www.notion.so/uwblueprintexecs/Task-Board-db95cd7b93f245f78ee85e3a8a6a316d)
    
    
    <!-- Give a quick summary of the implementation details, provide design
    justifications if necessary -->
    ## Implementation description
    * 
    
    
    <!-- What should the reviewer do to verify your changes? Describe
    expected results and include screenshots when appropriate -->
    ## Steps to test
    1.
    
    
    <!-- Draw attention to the substantial parts of your PR or anything
    you'd like a second opinion on -->
    ## What should reviewers focus on?
    * 
    
    
    ## Checklist
    - [x] My PR name is descriptive and in imperative tense
    - [x] My commit messages are descriptive and in imperative tense. My
    commits are atomic and trivial commits are squashed or fixup'd into
    non-trivial commits
    - [x ] I have run the appropriate linter(s)
    - [x] I have requested a review from the PL, as well as other devs who
    have background knowledge on this PR or who will be building on top of
    this PR

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from '@/components/ui/provider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <html suppressHydrationWarning>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </html>
+  );
 }
