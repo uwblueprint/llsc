2faded2a3ea1cdfb022ee0f62db0fe0c102bd85b
ebwu95 <38186996+ebwu95@users.noreply.github.com>
2025-03-23 20:32:00 -0400

    Fixed Frontend Dependency Issues (#31)
    ## Notion ticket link
    <!-- Please replace with your ticket's URL -->
    [Ticket
    Name](https://www.notion.so/uwblueprintexecs/Task-Board-db95cd7b93f245f78ee85e3a8a6a316d)
    
    
    <!-- Give a quick summary of the implementation details, provide design
    justifications if necessary -->
    ## Implementation description
    * 
    
    
    <!-- What should the reviewer do to verify your changes? Describe
    expected results and include screenshots when appropriate -->
    ## Steps to test
    1.
    
    
    <!-- Draw attention to the substantial parts of your PR or anything
    you'd like a second opinion on -->
    ## What should reviewers focus on?
    * 
    
    
    ## Checklist
    - [ ] My PR name is descriptive and in imperative tense
    - [ ] My commit messages are descriptive and in imperative tense. My
    commits are atomic and trivial commits are squashed or fixup'd into
    non-trivial commits
    - [ ] I have run the appropriate linter(s)
    - [ ] I have requested a review from the PL, as well as other devs who
    have background knowledge on this PR or who will be building on top of
    this PR

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,10 +4,8 @@ import { Provider } from '@/components/ui/provider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <html suppressHydrationWarning>
-      <Provider>
-        <Component {...pageProps} />
-      </Provider>
-    </html>
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
