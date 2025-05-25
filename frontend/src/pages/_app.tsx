4921f29767ee1f20897a89dc1aef8a7093c9299e
Lucy Qi <l8qi@uwaterloo.ca>
2025-05-25 15:58:55 -0400

    added open sans serif font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,10 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet" />
         <style>{`body { font-family: 'Open Sans', sans-serif; }`}</style>
       </Head>
       <Provider>
