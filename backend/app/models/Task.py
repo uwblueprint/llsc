70f9e59f252fef0f5cb5196212ed5a4c003fdddc
richieb21 <76607899+richieb21@users.noreply.github.com>
2025-10-02 21:37:14 -0400

    linting

--- backend/app/models/Task.py
+++ backend/app/models/Task.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 from enum import Enum as PyEnum
 
-from sqlalchemy import Column, DateTime, ForeignKey, String
+from sqlalchemy import Column, DateTime, ForeignKey
 from sqlalchemy import Enum as SQLEnum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
