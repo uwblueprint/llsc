ed4f608380c76dda1a83b86394e61d2db8853a74
Ethan Chen <e263chen@uwaterloo.ca>
2025-11-24 21:29:54 -0500

    ruff format

--- backend/migrations/versions/77423a45f740_merge_multiple_heads_drop_available_.py
+++ backend/migrations/versions/77423a45f740_merge_multiple_heads_drop_available_.py
@@ -5,11 +5,12 @@ Revises: 93cc5dac324a, dda4b46776e9
 Create Date: 2025-11-24 21:20:43.406772
 
 """
+
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = '77423a45f740'
-down_revision: Union[str, None] = ('93cc5dac324a', 'dda4b46776e9')
+revision: str = "77423a45f740"
+down_revision: Union[str, None] = ("93cc5dac324a", "dda4b46776e9")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- backend/migrations/versions/93cc5dac324a_merge_multiple_heads.py
+++ backend/migrations/versions/93cc5dac324a_merge_multiple_heads.py
@@ -5,11 +5,12 @@ Revises: 23dae9594e1d, 329a7ab72d38
 Create Date: 2025-11-24 19:04:33.167997
 
 """
+
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = '93cc5dac324a'
-down_revision: Union[str, None] = ('23dae9594e1d', '329a7ab72d38')
+revision: str = "93cc5dac324a"
+down_revision: Union[str, None] = ("23dae9594e1d", "329a7ab72d38")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
